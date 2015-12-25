474b90bbb97f8bd6c71d80a6aad8b3dbc2bc2424
Michael Kant <meiskant@gmail.com>
2015-12-24 16:11:24 -0800

    wins and losses are now recorded to userScores collection

--- server/serverMethods.js
+++ server/serverMethods.js
@@ -7,22 +7,37 @@ Meteor.methods({
         if (Object.keys(results).length > 3) {
         //if (results.length > 3) {
             for (var i in results) {
-                var won = false;
                 if (i == 'game' || i == 'team' || i == 'winner') {
                     continue;
                 }
                 else {
-                    if (results.winner == results[i]) {
-                        won = true;
+                    var incFaction = {};
+                    incFaction[results[i]] = 1;
+                    var gameUpdate = UserScores.findOne({userID: i, gameID: results['game']});
+                    if (gameUpdate) {
+                        if (results.winner == results[i]) {
+                            UserScores.update(gameUpdate, { $inc: incFaction });
+                        } else {
+                            UserScores.update(gameUpdate, { $inc: {losses: 1} });
+                        }
+                    } else {
+                        if (results.winner == results[i]) {
+                            var gameInsert = UserScores.insert({
+                                userID: i,
+                                gameID: results['game'],
+                                createdAt: new Date()
+                            });
+                            UserScores.update(gameInsert, { $inc: incFaction });
+                        } else {
+                            var gameInsert = UserScores.insert({
+                                userID: i,
+                                gameID: results['game'],
+                                faction: results[i],
+                                createdAt: new Date()
+                            });
+                            UserScores.update(gameInsert, { $inc: {losses: 1} });
+                        }
                     }
-                    var newScore = UserScores.insert({
-                        userID: i,
-                        gameID: results['game'],
-                        teamID: results['team'],
-                        won: won,
-                        faction: results[i],
-                        createdAt: new Date()
-                    });
                 }
             }
             return 'success';
